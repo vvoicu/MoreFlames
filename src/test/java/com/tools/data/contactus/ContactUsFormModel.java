078fe4ee9de07fcbcecaeb663592ba43b03e5860
BodeaCornelia <bodea_nelly@yahoo.com>
2016-10-17 09:42:32 +0300

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/vvoicu/MoreFlames
    
    Conflicts:
    	src/test/java/com/cucumber/stepdefinitions/navigation/NavigationSteps.java

---
+++ src/test/java/com/tools/data/contactus/ContactUsFormModel.java
@@ -0,0 +1,80 @@
+package com.tools.data.contactus;
+
+public class ContactUsFormModel {
+
+	private String fisrtNameInput;
+	private String surnameInput;
+	private String emailInput;
+	private String phoneInput;
+	private String countryDropDown;
+	private String enquiryDropDown;
+	private String subjectInput;
+	private String messageInput;
+	private String radioContactByEmail;
+	private String radioContactByPhone;
+	
+	
+	
+	public String getRadioContactByEmail() {
+		return radioContactByEmail;
+	}
+	public void setRadioContactByEmail(String radioContactByEmail) {
+		this.radioContactByEmail = radioContactByEmail;
+	}
+	public String getRadioContactByPhone() {
+		return radioContactByPhone;
+	}
+	public void setRadioContactByPhone(String radioContactByPhone) {
+		this.radioContactByPhone = radioContactByPhone;
+	}
+	public String getFisrtNameInput() {
+		return fisrtNameInput;
+	}
+	public void setFisrtNameInput(String fisrtNameInput) {
+		this.fisrtNameInput = fisrtNameInput;
+	}
+	public String getSurnameInput() {
+		return surnameInput;
+	}
+	public void setSurnameInput(String surnameInput) {
+		this.surnameInput = surnameInput;
+	}
+	public String getEmailInput() {
+		return emailInput;
+	}
+	public void setEmailInput(String emailInput) {
+		this.emailInput = emailInput;
+	}
+	public String getPhoneInput() {
+		return phoneInput;
+	}
+	public void setPhoneInput(String phoneInput) {
+		this.phoneInput = phoneInput;
+	}
+	public String getCountryDropDown() {
+		return countryDropDown;
+	}
+	public void setCountryDropDown(String countryDropDown) {
+		this.countryDropDown = countryDropDown;
+	}
+	public String getEnquiryDropDown() {
+		return enquiryDropDown;
+	}
+	public void setEnquiryDropDown(String enquiryDropDown) {
+		this.enquiryDropDown = enquiryDropDown;
+	}
+	public String getSubjectInput() {
+		return subjectInput;
+	}
+	public void setSubjectInput(String subjectInput) {
+		this.subjectInput = subjectInput;
+	}
+	public String getMessageInput() {
+		return messageInput;
+	}
+	public void setMessageInput(String messageInput) {
+		this.messageInput = messageInput;
+	}
+	
+	
+}
